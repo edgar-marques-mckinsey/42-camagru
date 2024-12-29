e38e63a49261c672c0157d567dd8b6556772f99c
edgar-marques-mckinsey <edgar_marques@mckinsey.com>
2024-12-29 23:22:55 +0000

    Add a common navbar

--- frontend/files/scripts/index.js
+++ frontend/files/scripts/index.js
@@ -1,3 +1,20 @@
+const navbar = `
+    <nav class="fixed top-0 left-0 flex items-center w-full bg-gray-800 text-white h-24 py-6 px-4">
+        <ul class="container flex items-center justify-end gap-6 mx-auto">
+            <li class="mr-auto">
+                <a href="/">
+                    <img src="/images/logo-42.svg" alt="Logo of School 42" class="h-10" />
+                </a>
+            </li>
+            <li>
+                <a href="/login.html" class="hover:underline">
+                    Login
+                </a>
+            </li>
+        </ul>
+    </nav>
+`;
+
 document.addEventListener('DOMContentLoaded', () => {
-    console.log('DOMContentLoaded!');
+    document.body.insertAdjacentHTML('afterbegin', navbar);
 });
