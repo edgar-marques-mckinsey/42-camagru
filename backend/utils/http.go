b5588b7873390971b456c937cdf3cc5f4dbc866a
edgar-marques-mckinsey <edgar_marques@mckinsey.com>
2024-12-31 22:29:17 +0000

    Create the SendMessage backend function

--- backend/utils/http.go
+++ backend/utils/http.go
@@ -21,3 +21,20 @@ func SendError(w http.ResponseWriter, msg string, status ...int) {
 	response := ErrorResponse{Message: msg}
 	json.NewEncoder(w).Encode(response)
 }
+
+type ValidResponse struct {
+	Data any `json:"data"`
+}
+
+func SendMessage(w http.ResponseWriter, data any, status ...int) {
+	statusCode := http.StatusOK
+	if len(status) > 0 {
+		statusCode = status[0]
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(statusCode)
+
+	response := ValidResponse{Data: data}
+	json.NewEncoder(w).Encode(response)
+}
