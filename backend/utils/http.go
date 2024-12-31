a59e35738174e3c1a7f7c89c82d7fc47899b76fb
edgar-marques-mckinsey <edgar_marques@mckinsey.com>
2024-12-31 21:01:01 +0000

    Update the SendError backend function

--- backend/utils/http.go
+++ backend/utils/http.go
@@ -1,11 +1,23 @@
 package utils
 
 import (
-	"log"
+	"encoding/json"
 	"net/http"
 )
 
-func SendError(w http.ResponseWriter, msg string) {
-	log.Println("Error:", msg)
-	http.Error(w, msg, http.StatusInternalServerError)
+type ErrorResponse struct {
+	Message string `json:"message"`
+}
+
+func SendError(w http.ResponseWriter, msg string, status ...int) {
+	statusCode := http.StatusInternalServerError
+	if len(status) > 0 {
+		statusCode = status[0]
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(statusCode)
+
+	response := ErrorResponse{Message: msg}
+	json.NewEncoder(w).Encode(response)
 }
