2181725a9383ddfd9c27ba2a385eb28ed4984c2d
Edgar Marques <dev@weaccelerategrowth.com>
2025-02-17 20:45:52 +0000

    Fix the SendEmail function

--- backend/utils/email.go
+++ backend/utils/email.go
@@ -1,11 +1,28 @@
 package utils
 
-import "log"
+import (
+	"net/smtp"
+	"os"
+)
 
 func SendEmail(toEmail, subject, body string) error {
-	log.Println("toEmail:", toEmail)
-	log.Println("subject:", subject)
-	log.Println("body:", body)
+	from := os.Getenv("EMAIL_ADDRESS")
+	fromPass := os.Getenv("EMAIL_PASSWORD")
+	smtpHost := os.Getenv("EMAIL_HOST")
+
+	smtpPort := "587"
+	auth := smtp.PlainAuth("", from, fromPass, smtpHost)
+
+	to := []string{toEmail}
+	message := []byte(
+		"Subject: Camagru - " + subject + "\r\n\r\n" +
+			body + "\r\n",
+	)
+
+	err := smtp.SendMail(smtpHost+":"+smtpPort, auth, from, to, message)
+	if err != nil {
+		return nil
+	}
 
 	return nil
 }
