0090808a435fd2fc953e21dbc2ef2f69068c52d7
Edgar Marques <edgar.eu1@hotmail.com>
2025-02-18 00:17:39 +0000

    Create the receive_comment_notifications User field #2

--- database/docker-entrypoint-initdb.d/01-init.sql
+++ database/docker-entrypoint-initdb.d/01-init.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS users (
     email VARCHAR(100) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
     was_email_verified BOOLEAN DEFAULT FALSE,
-    receive_comment_notifications BOOLEAN DEFAULT TRUE
+    receive_comment_notifications BOOLEAN DEFAULT TRUE,
     verification_code VARCHAR(16) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
